5347391545cc34069a1180d91397fed80cde3acb
shaylanb24 <shaylanbhagat@gmail.com>
2026-04-06 13:22:51 +0800

    acp.py

---
+++ acp.py
@@ -0,0 +1,8 @@
+shaylan_birthday = "June 24"
+print(f"Shaylan's birthday is on {shaylan_birthday}")
+shaylan_dog_birthday = "May 24"
+print(f"Shaylan's dog's birthday is on {shaylan_dog_birthday}")
+shaylan_mother_birthday = "October 8"
+print(f"Shaylan's mother's birthday is on {shaylan_mother_birthday}")
+shaylan_dad_birthday = "July 6"
+print(f"Shaylan's dad's birthday is on {shaylan_dad_birthday}")
