bc2079c88e34f0299e3c6a663fce59b020544787
shaylanb24 <shaylanbhagat@gmail.com>
2026-04-13 18:12:24 +0800

    acp L3

---
+++ datatypes.py
@@ -0,0 +1,14 @@
+a = "Shaylan"
+print(type(a))
+b = 8
+print(type(b))
+c = 10.11
+print(type(c))
+d = True
+print(type(d))
+
+age = 8
+print(type(str(age)))
+print(a[0:5])
+name2 =  "vidyullatha"
+print(name2[0:5])
