d9c58b3af8d9c3b5cefd75fdbdcf818c1c9d5f17
shaylanb24 <shaylanbhagat@gmail.com>
2026-04-26 13:25:02 +0800

    L5

---
+++ conditionalstatements.py
@@ -0,0 +1,5 @@
+Number=int(input("enter your number"))
+if(Number>50):
+    print("You are granpa" ,)
+else:
+    print ("you are kid")
---
+++ indentation.py
@@ -0,0 +1,10 @@
+Number=24
+
+if(Number>10):
+
+    print("Shaylan is" ,Number)
+
+number=6
+
+if(number<10):
+    print(" shaylan is" ,number)
